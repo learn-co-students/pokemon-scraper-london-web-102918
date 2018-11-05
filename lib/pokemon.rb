2d9b6d180a466de4d0ea6ab3daa2a0b868d9bf31
artismarti <artismarti@gmail.com>
2018-11-05 19:00:45 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,11 @@
 class Pokemon
   attr_accessor :id, :name, :type, :hp, :db
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, hp: nil, db:)
     @id = id
     @name = name
     @type = type
+    @hp = hp
     @db = db
   end
 
@@ -14,6 +15,11 @@ class Pokemon
 
   def self.find(id_num, db)
     pokemon_found = db.execute("SELECT * FROM pokemon WHERE ID=?", id_num).flatten
-    Pokemon.new(id: pokemon_found[0], name: pokemon_found[1], type: pokemon_found[2], db: db)
+    Pokemon.new(id: pokemon_found[0], name: pokemon_found[1], type: pokemon_found[2], hp: pokemon_found[3], db: db)
   end
+
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE name = ?", hp, self.name)
+  end
+
 end
